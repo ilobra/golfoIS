7de33edb9113633051aa2a467d8ce7928583a5f2
ilobra <ilona.brazauskaite@gmail.com>
2017-12-11 15:22:18 +0200

    Mokėjimų paieška pagal asmens el. paštą, stiliaus pritaikymai

--- src/AppBundle/Controller/MokejimaiController.php
+++ src/AppBundle/Controller/MokejimaiController.php
@@ -33,13 +33,14 @@ class MokejimaiController extends Controller
      * Lists all mokejimai entities.
      *
      * @Route("/iranga", name="uziranga")
-     * @Method("GET")
+     * @Method({"GET", "POST"})
      */
     public function irangosMokejimaiAction(Request $request)
     {
 
         $datanuo=$request->query->get('from');
         $dataiki=$request->query->get('to');
+        $searchemail=$request->request->get('emailSearch');
         $em = $this->getDoctrine()->getManager();
 
         $parameters = array(
@@ -55,10 +56,21 @@ class MokejimaiController extends Controller
             		AND n.id=a.id AND ia.isnuomojimoPradzia >= :nuo AND ia.isnuomojimoPradzia <= :iki
             ')->setParameters($parameters);
 
+
         $results = $sql->getResult();
+
+        $sqlemail=$em->createQuery('SELECT ia.id, ia.suma, ia.isnuomojimoPradzia, ia.isnuomojimoPabaiga, 
+            		a.vardas, a.pavarde, a.elPastas, a.asmensKodas, a.id AS narioId, 
+            		n.bankoKortNumeris, it.name AS iranga, i.kokybe 
+            		FROM AppBundle:IrangosApmokejimas ia, AppBundle:Iranga i, AppBundle:Narys n, AppBundle:IrangosTipas it, AppBundle:Asmuo a
+            		WHERE ia.fkIrangaid=i.id AND ia.fkNarysid=n.id AND i.tipas=it.idIrangosTipas
+            		AND n.id=a.id AND a.elPastas=:searchEmail
+            ')->setParameter('searchEmail',$searchemail);
+        $resultsemail=$sqlemail->getResult();
 
         return $this->render('mokejimai/uziranga.html.twig', array(
             'mokejimai' => $results,
+            'asmensmokejimai'=>$resultsemail
         ));
     }
 
@@ -66,12 +78,13 @@ class MokejimaiController extends Controller
      * Lists all mokejimai entities.
      *
      * @Route("/naryste", name="uznaryste")
-     * @Method("GET")
+     * @Method({"GET", "POST"})
      */
     public function narystesMokejimaiAction(Request $request)
     {
         $datanuo=$request->query->get('from');
         $dataiki=$request->query->get('to');
+        $searchemail=$request->request->get('emailSearch');
         $em = $this->getDoctrine()->getManager();
 
         $sql = $em->createQuery('
@@ -85,9 +98,20 @@ class MokejimaiController extends Controller
 
         $results = $sql->getResult();
 
+        $sqlemail = $em->createQuery('
+            		SELECT na.id, a.id AS narioId, n.bankoKortNumeris, a.vardas, a.pavarde,a.elPastas, a.asmensKodas,
+            		na.suma, na.narystesPradzia,na.narystesPabaiga
+            		FROM AppBundle:Narys n, AppBundle:Asmuo a, AppBundle:NarystesApmokejimas na
+            		WHERE n.id=a.id AND n.id=na.fkNarysid
+            		AND a.elPastas=:searchEmail
+            ')->setParameter('searchEmail',$searchemail);
+
+        $resultsemail = $sqlemail->getResult();
+
 
         return $this->render('mokejimai/uznaryste.html.twig', array(
             'narystes' => $results,
+            'asmensnarystes'=>$resultsemail,
 
         ));
     }
